b31758b08a9bd74845329736b53e972d97758679
chenjiahau <chenjiahau@gmail.com>
2023-04-19 16:31:52 +0800

    Draft tools

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState({
+    email: '',
+    name: '',
+    password: '',
+    gender: 'm'
+  });
+
+  const changeValue = (e) => {
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const checkForm = () => {
+    if (value.email === '' || value.name === '' || value.password === '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const handleSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input type="text" className="form-control" id="email" name="email" autoComplete="new-password" onChange={changeValue} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" id="name" name="name" autoComplete="new-password" onChange={changeValue} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" name="password" autoComplete="new-password" onChange={changeValue} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="gender">Gender</label>
+          <select className="form-control" name="gender">
+            <option value="m">M</option>
+            <option value="f">F</option>
+          </select>
+        </div>
+        <div className="form-group form-group--button">
+          <button className="form-group--align-right" disabled={checkForm()}>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
---
+++ client/src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+ 
+const firebaseConfig = {
+ apiKey: "AIzaSyDX2bvmGb6u_HbyB3XYKRa5M4I0nwuxlk0",
+ authDomain: "person-applicaton-system.firebaseapp.com",
+ projectId: "person-applicaton-system",
+ storageBucket: "person-applicaton-system.appspot.com",
+ messagingSenderId: "871609677817",
+ appId: "1:871609677817:web:2e6473e9a5f38641bcaf28"
+};
+ 
+initializeApp(firebaseConfig);
